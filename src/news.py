dde74818cd8d1b418ae6ce890432b2d7dcc99db2
Jose Alonso Leon <jleon@sandiegobloodbank.org>
2018-09-30 13:12:23 -0700

    Using the top headlines and formatting the news into an array

--- src/news.py
+++ src/news.py
@@ -1,12 +1,23 @@
 import requests
+import json
 class News :
     def __init__(self, token) :
         self.URL = "https://newsapi.org/v2/"
         self.token = token
 
     def topNewsHeadlines(self):
-        PARAMS = { 'country' : 'us', 'apiKey' : self.token }
+        PARAMS = { 'country' : 'us', 'apiKey' : self.token, 'category' : 'general'}
         r = requests.get(url=self.URL + 'top-headlines', params=PARAMS)
-        print(r.json())
-        return data
+        news = r.json()
+        return news
+
+    def formatNews(jsonNews) :
+        formatNews = []
+        for new in jsonNews :
+            formatNews['source'] = new.source.name
+            formatNews['author'] = new.author
+            formatNews['title'] = new.title
+            formatNews['description'] = new.description
+
+        return formatNews
 
